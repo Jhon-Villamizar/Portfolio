95ba0a0c1d11b8a072c8a2328a4212dbb0664398
Jhon Villamizar <jhonvio@hotmail.com>
2022-03-18 00:59:54 -0500

    configuration

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => (
+  <>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint suscipit
+      adipisci hic omnis maiores doloribus at corporis nihil vitae. Laudantium,
+      nesciunt dolores doloremque aspernatur commodi impedit vitae dolor sit
+      voluptas.
+    </p>
+  </>
+);
+
+export default App;
