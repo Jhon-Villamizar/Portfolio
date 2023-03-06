e06ed24315b7a1cf135dc07a51abeba922c25cb8
Jhon Villamizar <jhonvio@hotmail.com>
2023-03-06 11:14:08 -0500

    add: image in home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,15 @@ import './header.scss'
 const Header = () => {
   return (
     <div className='container-header'>
-      <h1>
-        Hello, i am <br />
-        Jhon Villamizar
-      </h1>
-      <h4>Software developer fullstack</h4>
-      <div>
-        <img src='' alt='' />
+      <div className='header-info'>
+        <h1>
+          Hello, i am <br />
+          Jhon Villamizar
+        </h1>
+        <h4>Software developer fullstack</h4>
+      </div>
+      <div className='header-img'>
+        <img src='assets/2.png' alt='' />
       </div>
     </div>
   )
