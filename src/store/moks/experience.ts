3bb781ab3cfd6f765a1b560be8e417445638b4f5
jhon <jhonvio@hotmail.com>
2023-03-20 01:31:35 -0500

    add: change img path

--- src/store/moks/experience.ts
+++ src/store/moks/experience.ts
@@ -3,8 +3,8 @@ export const Experience = [
     company: 'Devsavant',
     position: 'Software Developer',
     time: 'Apr 2022 / Nov 2022',
-    img: 'src/img/svg/devsavant.svg',
-    img2: 'src/img/svg/devsavantWhite.svg',
+    img: '../../img/svg/devsavant.svg',
+    img2: '../../img/svg/devsavantWhite.svg',
     description:
       'Build, maintain and update different components and packages for the company Impartner from the United States, with technologies such as Angular, Knockout, Sass, Typescript, Agil, Kamban.',
   },
@@ -12,8 +12,8 @@ export const Experience = [
     company: 'Perficient',
     position: 'Software Developer',
     time: 'Nov 2021 / Apr 2022',
-    img: 'src/img/svg/perficient.svg',
-    img2: 'src/img/svg/perficientWhite.svg',
+    img: '../../img/svg/perficient.svg',
+    img2: '../../img/svg/perficientWhite.svg',
     description:
       'Build web applications with different frontend frameworks such as Angular and React, creating components, tests, storage, styles, and consuming services to get data and get data with graphQL. Build API with NodeJS, Express, non-relational databases, and deployed in AWS as serverless with lambda functions and dynamoDB',
   },
@@ -21,8 +21,8 @@ export const Experience = [
     company: 'Globant',
     position: 'Web UI Developer',
     time: 'Nov 2021 / Apr 2022',
-    img: 'src/img/svg/globant.svg',
-    img2: 'src/img/svg/globantWhite.svg',
+    img: '../../img/svg/globant.svg',
+    img2: '../../img/svg/globantWhite.svg',
     description:
       'Build web applications with different frontend frameworks such as Angular and React, creating components, tests, storage, styles, and consuming services to get data.',
   },
@@ -38,7 +38,7 @@ export const Experience = [
     company: 'Chef Company',
     position: 'Frontend Developer',
     time: 'Jul 2019 / Apr 2020',
-    img: 'src/img/svg/isobar.svg',
+    img: '../../img/svg/isobar.svg',
     description:
       'Build custom web applications with HTML, CSS, JavaScript, ReactJs, Angular+2, NodeJs, and consume API REST services. ',
   },
@@ -46,8 +46,8 @@ export const Experience = [
     company: 'Pratech',
     position: 'Backend Developer',
     time: 'Aug 2018 / Jul 2019',
-    img: 'src/img/svg/pratech.svg',
-    img2: 'src/img/svg/pratechWhite.svg',
+    img: '../../src/img/svg/pratech.svg',
+    img2: '../../img/svg/pratechWhite.svg',
     description:
       'Build custom applications in the Backend with NodeJs in TypeScript, C# .NET, Java Spring Boot, and MongoDB. Build custom applications in the Frontend Angular+2, ReactJs. Create and consume APIs and CRUDs deployed on IBM Cloud Foundry.',
   },
