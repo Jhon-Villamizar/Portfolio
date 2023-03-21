c8754a2c3756167d7c4f7d57899d6b5a35b6e114
jhon <jhonvio@hotmail.com>
2023-03-20 23:43:47 -0500

    add: change style card experience

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
 import './card.scss'
 
 type typeProps = {
   company: string
   position?: string
   time: string
-  description: string
   img?: string
 }
 
-const Index = ({ company, position, time, description, img }: typeProps) => {
-  const [text, setText] = useState(false)
-
+const Index = ({ company, position, time, img }: typeProps) => {
   return (
-    <div
-      className='card-content'
-      onMouseEnter={() => setText(true)}
-      onMouseLeave={() => setText(false)}
-    >
-      {text ? (
-        <p className='description'>{description}</p>
-      ) : (
-        <>
-          {img ? <img src={img} /> : <h6>{company}</h6>}
-          <div className='card-info'>
-            <p>{position}</p>
-            <p>{time}</p>
-          </div>
-        </>
-      )}
+    <div className='card-content'>
+      {img ? <img src={img} /> : <h6>{company}</h6>}
+      <div className='card-info'>
+        <p>{position}</p>
+        <p>{time}</p>
+      </div>
     </div>
   )
 }
--- src/components/InfoExperience/index.tsx
+++ src/components/InfoExperience/index.tsx
@@ -20,8 +20,6 @@ export const Experience = [
     time: 'Apr 2022 / Nov 2022',
     img: dev,
     img2: devWhite,
-    description:
-      'Build, maintain and update different components and packages for the company Impartner from the United States, with technologies such as Angular, Knockout, Sass, Typescript, Agil, Kamban.',
   },
   {
     company: 'Perficient',
@@ -29,8 +27,6 @@ export const Experience = [
     time: 'Nov 2021 / Apr 2022',
     img: per,
     img2: perWhite,
-    description:
-      'Build web applications with different frontend frameworks such as Angular and React, creating components, tests, storage, styles, and consuming services to get data and get data with graphQL. Build API with NodeJS, Express, non-relational databases, and deployed in AWS as serverless with lambda functions and dynamoDB',
   },
   {
     company: 'Globant',
@@ -38,24 +34,18 @@ export const Experience = [
     time: 'Nov 2021 / Apr 2022',
     img: glo,
     img2: gloWhite,
-    description:
-      'Build web applications with different frontend frameworks such as Angular and React, creating components, tests, storage, styles, and consuming services to get data.',
   },
   {
     company: 'NetConsult',
     position: 'Full Stack Developer',
     time: 'May 2020 / Sep 2020',
     img: '',
-    description:
-      'Create and build web applications with Angular+2, NodeJs, Firebase. Create and build mobile applications with NodeJs, Firebase, and Ionic.',
   },
   {
     company: 'Chef Company',
     position: 'Frontend Developer',
     time: 'Jul 2019 / Apr 2020',
     img: iso,
-    description:
-      'Build custom web applications with HTML, CSS, JavaScript, ReactJs, Angular+2, NodeJs, and consume API REST services. ',
   },
   {
     company: 'Pratech',
@@ -63,8 +53,6 @@ export const Experience = [
     time: 'Aug 2018 / Jul 2019',
     img: pra,
     img2: praWhite,
-    description:
-      'Build custom applications in the Backend with NodeJs in TypeScript, C# .NET, Java Spring Boot, and MongoDB. Build custom applications in the Frontend Angular+2, ReactJs. Create and consume APIs and CRUDs deployed on IBM Cloud Foundry.',
   },
 ]
 
@@ -86,7 +74,6 @@ const InfoExperience = () => {
                 company={item.company}
                 position={item.position}
                 time={item.time}
-                description={item.description}
                 img={item.img2 && theme === 'dark' ? item.img2 : item.img}
               />
             </li>
