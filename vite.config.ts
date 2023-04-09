ac7791d58c3d8c2091f0e06f6ca63f4b803f83ba
jhon <jhonvio@hotmail.com>
2023-04-09 18:13:57 -0500

    add: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import imageminPlugin from 'vite-plugin-imagemin'
 
 //https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    reactRefresh(),
+    imageminPlugin({
+      svgo: {
+        plugins: [{ removeViewBox: false }, { cleanupIDs: false }],
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+      },
+    }),
+  ],
   base: '/Portfolio',
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    ssr: false,
+  },
 })
