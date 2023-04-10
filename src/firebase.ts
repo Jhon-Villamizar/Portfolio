a9ae344fa4196a1e4fac1353cd7b725d7bbc3508
jhon <jhonvio@hotmail.com>
2023-04-09 19:42:29 -0500

    add: add enviroment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,9 @@
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAn-n-AG4zz4t_EiEq6aBOwIvLx2zKx8SI',
-  authDomain: 'portfolio-d5950.firebaseapp.com',
-  projectId: 'portfolio-d5950',
-  storageBucket: 'portfolio-d5950.appspot.com',
-  messagingSenderId: '669791356717',
-  appId: '1:669791356717:web:1210b20e7b86e8543d75a4',
-  measurementId: 'G-L4VPGVDW02',
-}
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG as string)
 
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import imageminPlugin from 'vite-plugin-imagemin'
+import dotenv from 'dotenv'
 
-//https://vitejs.dev/config/
+dotenv.config()
+
 export default defineConfig({
   plugins: [
     react(),
